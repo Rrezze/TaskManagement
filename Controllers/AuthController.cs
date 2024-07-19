fa69841322058ed605c72dc5c5fb1b4264561849
Rrezze <88327916+Rrezze@users.noreply.github.com>
2024-07-19 12:38:31 +0200

    userAuth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using taskManagement.Models;
+using taskManagement.Models.DTO;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace taskManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(IConfiguration configuration, ILogger<AuthController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.Email = request.Email;
+            user.PasswordHash = passwordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var tokenValue = _configuration.GetSection("AppSettings:Token").Value;
+            if (string.IsNullOrEmpty(tokenValue))
+            {
+                _logger.LogError("Token value is not configured.");
+                throw new ArgumentNullException("Token value is not configured.");
+            }
+            _logger.LogInformation("Token value retrieved: {TokenValue}", tokenValue);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenValue));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
